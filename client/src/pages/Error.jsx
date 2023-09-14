72cc3016af8ee4df445c4a39a7c9bc13c88461e7
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-14 14:50:52 +0530

    Error page and styled component added

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,11 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import notFound from "../assets/images/not-found.svg";
 
 const Error = () => {
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={notFound} alt='not found' />
+          <h3>Ohh! page not found</h3>
+          <p>We can&apos;t seem to find the page you rae looking for</p>
+          <Link to='/dashboard'>Back Home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to='/'>Back Home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong!</h3>
+      </div>
+    </Wrapper>
   );
 };
 
