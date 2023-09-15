8312e756d9ff97f97288b9d93cda67cc1c9f6340
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-15 20:30:35 +0530

    logout and styled component added

---
+++ client/src/components/LogoutContainer.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { FaCaretDown, FaUserCircle } from "react-icons/fa";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import Wrapper from "../assets/wrappers/Logout";
+
+const LogoutContainer = () => {
+  const [showLogout, setShowLogout] = useState();
+  const { user, logoutUser } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <button
+        type='button'
+        className='btn logout-btn'
+        onClick={() => setShowLogout(!showLogout)}
+      >
+        <FaUserCircle />
+        {user?.name}
+        <FaCaretDown />
+      </button>
+      <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+        <button type='button' className='dropdown-btn' onClick={logoutUser}>
+          logout
+        </button>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default LogoutContainer;
