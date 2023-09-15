be2d5658fc0614defa48ae28a1c24a2f37962205
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-15 19:10:30 +0530

    small sidebar and styled component added

---
+++ client/src/components/SmallSidebar.jsx
@@ -0,0 +1,30 @@
+import { FaTimes } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import Logo from "./Logo";
+import { useDashboardContext } from "./../pages/DashboardLayout";
+import NavLinks from "./NavLinks";
+
+const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default SmallSidebar;
