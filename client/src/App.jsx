732478722cfc7aecf94ad719b7b655417d018486
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-16 00:06:42 +0530

    added dark theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,14 @@ import AllJobs from "./pages/AllJobs";
 import Profile from "./pages/Profile";
 import Admin from "./pages/Admin";
 
+export const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem("darkTheme") === "true";
+  document.body.classList.toggle("dark-theme", isDarkTheme);
+  return isDarkTheme;
+};
+
+checkDefaultTheme();
+
 const router = createBrowserRouter([
   {
     path: "/",
--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -5,15 +5,21 @@ import Wrapper from "../assets/wrappers/Dashboard";
 import SmallSidebar from "../components/SmallSidebar";
 import BigSidebar from "../components/BigSidebar";
 import Navbar from "../components/Navbar";
+import { checkDefaultTheme } from "../App";
 
 const DashboardContext = createContext();
 
 const DashboardLayout = () => {
   const user = { name: "raviranjan" };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme);
 
-  const toggleDarkTheme = () => console.log("Toggle dark theme");
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
 
   const toggleSidebar = () => setShowSidebar(!showSidebar);
 
